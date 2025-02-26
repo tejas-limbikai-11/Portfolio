94ef376e6a864619b211370dd2cc3fe2216c4d8f
tejas-limbikai-11 <118275996+tejas-limbikai-11@users.noreply.github.com>
2025-02-27 03:49:06 +0530

    Fix: Disable ESLint during builds

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,12 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
     ...compat.extends("next/core-web-vitals", "next/typescript"),
-    {
-        rules: {
-            "@typescript-eslint/no-unused-vars": "warn", // Change from "error" to "warn"
-            "prefer-const": "off", // Disable prefer-const rule
-        },
-    },
+    // {
+    //     rules: {
+    //         "@typescript-eslint/no-unused-vars": "warn", // Change from "error" to "warn"
+    //         "prefer-const": "off", // Disable prefer-const rule
+    //     },
+    // },
 ];
 
 export default eslintConfig;
